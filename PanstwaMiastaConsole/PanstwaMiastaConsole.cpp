e0d87bd49b0a87b99ae97b388b249771c08829c7
maciejek052 <maciejek052@gmail.com>
2021-11-02 00:24:41 +0100

    dodano wczytywanie odpowiedzi z klawiatury

--- PanstwaMiastaConsole/PanstwaMiastaConsole.cpp
+++ PanstwaMiastaConsole/PanstwaMiastaConsole.cpp
@@ -35,13 +35,11 @@ void initColorPairs() {
 }
 
 void drawRandomIndexes() {
-    for (int i = 0; i < numberOfRounds; i++) {
+    while (randomIndexes.size() < numberOfRounds) {
         int random = rand() % 22;
         // ensure that every letter will be unique
         if (find(randomIndexes.begin(), randomIndexes.end(), random) == randomIndexes.end())
             randomIndexes.push_back(random);
-        // DO NAPRAWIENIA!!! BO JAK WARTOSC SIE POWTARZA TO NIC SIE NIE DZIEJE
-        // ZAMIENIC NA WHILE CZY COS
     }
 }
 
@@ -82,7 +80,6 @@ void mainMenu() {
     print_centered(stdscr, 7, "Autor: Maciej Czech"); 
     attroff(COLOR_PAIR(2));
     // create a window for input
-    getmaxyx(stdscr, yMax, xMax);
     WINDOW* menuwin = newwin(7, xMax - 12, yMax - 8, 5);
     box(menuwin, 0, 0);
     refresh();
@@ -197,18 +194,103 @@ void startingGameAnimation() {
 }
 
 void game() {
-    // int currentRound = 0;
-    for (size_t i = 0; i < randomIndexes.size(); i++) {
-        // currentRound = static_cast<int>(i); 
+    // vectors to keep possible answers for each category
+    vector<string> possibleAnswers1, possibleAnswers2, possibleAnswers3, possibleAnswers4, possibleAnswers5, possibleAnswers6;
+    vector<string> allGivenAnswers;
+    string input[6]; 
+    int playerPoints = 0;
+    int botsPoints[5] = { 0 };
+    string choices[7] = { "Panstwo:", "Miasto:", "Zwierze:", "Zawod:",  "Owoc lub warzywo:", "Kolor:", "ZAKONCZ RUNDE" };
+    for (size_t x = 0; x < randomIndexes.size(); x++) { // iterate through every round
+        for (auto& a : input) a = ""; // clear input array
         clear();
-        letterAnimation(randomIndexes[i]); 
+        int letterIndex = randomIndexes[x]; 
+        letterAnimation(letterIndex);
+        clear();
+        string line;
+        ifstream roundArt("assets\\asciiart\\round.txt");
+        attron(COLOR_PAIR(1)); // green text on black bg
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // TO FIX BECAUSE IT SHOWS ROUND 1 EVERY TIME
+
+        for (int i = 0; i < 6; i++) { 
+            getline(roundArt, line);
+            print_centered(stdscr, i, line);
+            refresh();
+        }
+        string letterMessage = "Wylosowana litera: ";
+        letterMessage.push_back(alphabet[letterIndex] - 32); 
+        attron(COLOR_PAIR(2));
+        print_centered(stdscr, 8, letterMessage); 
+        attroff(COLOR_PAIR(2));
+        WINDOW* gamewin = newwin(9, xMax - 12, 11, 5);
+        box(gamewin, 0, 0);
+        refresh();
+        wrefresh(gamewin);
+        keypad(gamewin, true);
+        int choice;
+        int highlight = 0;
+        noecho();
+        while (1) {
+            wclear(gamewin); 
+            box(gamewin, 0, 0);
+            for (int i = 0; i < 7; i++) {
+                if (i == highlight)
+                    wattron(gamewin, A_REVERSE);
+                print_centered(gamewin, i+1, choices[i] + " " + input[i]);
+                wattroff(gamewin, A_REVERSE);
+                refresh();
+                wrefresh(gamewin);
+            }
+            choice = wgetch(gamewin);
+            switch (choice) {
+            case KEY_UP:
+                highlight--;
+                if (highlight == -1)
+                    highlight = 0;
+                break;
+            case KEY_DOWN:
+                highlight++;
+                if (highlight == 7)
+                    highlight = 6;
+                break;
+            case KEY_LEFT: // if user accidentally hits this button
+                break;
+            case KEY_RIGHT:
+                break; 
+            case 8: // backspace
+                if (!input[highlight].empty()) {
+                    input[highlight].pop_back(); 
+                }
+                break;
+            case 10: // enter 
+                break; 
+            default:
+                // it should be changed to only accept cases 65-122 (ascii characters)
+                if (highlight != 6) {
+                    input[highlight] += choice; 
+                }
+            }
+            if (choice == 10 && highlight == 6) {
+                flash(); // neat effect
+                break;
+            }
+        }
+        clear();
+        string a; 
+        for (int i = 0; i < 6; i++) {
+            a = choices[i] + " " + input[i] + '\n';
+            printw(a.data());
+        }
+        getch();
     }
 }
-
 
 int main()
 {
     initscr(); // start ncurses mode
+    getmaxyx(stdscr, yMax, xMax);
     initColorPairs(); // initialize color pairs used in program
     mainMenu(); // print main menu
     startingGameAnimation(); // shows what game options user picked
